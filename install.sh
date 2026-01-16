205924f13f9328f9237bb3b94c5e6fd6867c0430
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-01-16 12:46:50 +0500

    Initial zsh configuration and setup script with Oh My Zsh and plugins

--- install.sh
+++ install.sh
@@ -10,27 +10,37 @@ fi
 
 # 2. Install all apps from your list
 if [ -f "pkglist.txt" ]; then
+    echo "Installing packages..."
     yay -S --needed --noconfirm - < pkglist.txt
 fi
 
 # 3. Restore Konsole Settings
+echo "Restoring Konsole profiles and themes..."
 mkdir -p ~/.local/share/konsole
 mkdir -p ~/.local/share/kxmlgui5/konsole
-cp konsole/* ~/.local/share/konsole/
-cp konsole/konsoleui.rc ~/.local/share/kxmlgui5/konsole/
-cp konsolerc ~/.config/konsolerc
+cp konsole/* ~/.local/share/konsole/ 2>/dev/null
+cp konsole/konsoleui.rc ~/.local/share/kxmlgui5/konsole/ 2>/dev/null
+cp konsolerc ~/.config/konsolerc 2>/dev/null
 
 # 4. Restore autocommit script
+echo "Restoring custom scripts..."
 mkdir -p ~/.local/bin
 cp scripts/autocommit.py ~/.local/bin/autocommit
 chmod +x ~/.local/bin/autocommit
 
 # 5. Setup Zsh (Oh My Zsh + Plugins)
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-echo "Done! Restart Konsole to see your themes and profiles."
+# Clone plugins if they don't exist
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# 6. Restore .zshrc
+echo "Applying .zshrc configuration..."
+cp .zshrc ~/.zshrc
+
+echo "Setup Complete! Please restart Konsole."
